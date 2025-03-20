5ada5356ac8682d464043fc28fce984d1e91df74
Doan Luc Quoc Duy <doanduycb02az@gmail.com>
2025-03-20 13:09:19 +0700

    Kiem Tra

---
+++ db.php
@@ -0,0 +1,9 @@
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "qlsv";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
